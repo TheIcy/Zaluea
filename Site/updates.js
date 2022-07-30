e33b66fcf4b9e491f254c7e2e6f54e490ecf87a7
catc24 <86363077+catc24@users.noreply.github.com>
2022-07-29 20:36:01 -0400

    Update updates.js

--- Site/updates.js
+++ Site/updates.js
@@ -1,5 +1,5 @@
 var updates = [
-    {message: "Welcome to the first official release of Zaluea!"}
+    {message: "Welcome to the 3rd official release of Andyum!"}
 ];
 var updatespage = document.getElementById("updatespage");
 for(let item of updates) {
